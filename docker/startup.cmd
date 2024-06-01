af2c6e020d803a2b2277370aeee7f84e3ed81888
x-mago <mago@hsga.de>
2024-06-01 22:42:05 +0200

    fixes

--- docker/startup.cmd
+++ docker/startup.cmd
@@ -2,3 +2,4 @@ docker-compose -f docker\compose.yaml up -d
 timeout /t 5 /nobreak >nul
 docker exec laravel_quotes php artisan migrate
 docker exec laravel_quotes php artisan jetstream:install livewire --teams
+docker exec laravel_quotes php artisan db:seed UserSeeder
