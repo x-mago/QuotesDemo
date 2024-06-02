e17e10b891e32266903d5c15b88b25bc609cafaa
x-mago <mago@hsga.de>
2024-06-02 18:02:57 +0200

    Error-Handling

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -5,12 +5,17 @@ namespace App\Http\Controllers;
 use App\Models\Quotes;
 use Illuminate\Http\Request;
 use App\Http\Helpers\QuotesHelper;
+use Illuminate\Contracts\Encryption\DecryptException;
 
 class HomeController extends Controller
 {
     public function dashboard() {
         $quotes = Quotes::orderByDesc('created_at')->limit(4)->get();
-        return view('zitate')->with('quotes', $quotes);
+        try {
+            return view('zitate')->with('quotes', $quotes);
+        } catch(DecryptException $e) {
+            return view('error-decrypt')->with('error', $e->getMessage());            
+        }
     }
 
     public function update() {
